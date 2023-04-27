83d536c2928b00afd75c0f325573294409ac5904
Bkillz15 <brandonuhl@hotmail.com>
2023-04-27 10:31:59 -0400

    heroku-2

--- gbapi/urls.py
+++ gbapi/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from .views import GuestView, GetGuest, GetName, GuestNameID, UpdateGuest
 
 urlpatterns = [
+    path('', GuestView.as_view()),
     path('getIDs',GuestNameID.as_view()),
     path('get-guest',GetGuest.as_view()),
     path('get-name',GetName.as_view()),
     path('update-guest',UpdateGuest.as_view())
 ]
-
-# path('', GuestView.as_view()),
