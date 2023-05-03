e6e7c94a24abd3f05fcce675dbc93df1f431cc20
Bkillz15 <brandonuhl@hotmail.com>
2023-05-03 09:21:20 -0400

    heroku database

--- kuhlwedding2023/production.py
+++ kuhlwedding2023/production.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -95,16 +96,16 @@ WSGI_APPLICATION = 'kuhlwedding2023.wsgi.application'
 
 # hostname = os.environ['DBHOST']
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
+#         'ENGINPIPE': 'django.db.backends.postgresql',
 #         'NAME': os.environ['DBNAME'],
 #         'HOST': hostname + ".postgres.database.azure.com",
 #         'USER': os.environ['DBUSER'],
@@ -112,6 +113,8 @@ DATABASES = {
 #     }
 # }
 
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
